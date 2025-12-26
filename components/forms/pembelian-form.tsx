ffc5d850406b358423402387cba77e0786a4f69e
wahyulamani03 <wahyulamani03@gmail.com>
2025-12-26 09:06:57 +0000

    excepting quote for error deploying

--- components/forms/pembelian-form.tsx
+++ components/forms/pembelian-form.tsx
@@ -447,7 +447,7 @@ export function PembelianForm({ defaultNomorPo, suppliers, bahanBaku }: Pembelia
       {status === "received" && (
         <div className="p-4 bg-yellow-50 border border-yellow-200 rounded-lg">
           <p className="text-sm text-yellow-900">
-            ⚠️ <strong>Perhatian:</strong> Status "Diterima" akan langsung mengupdate stok dan harga beli bahan baku.
+            ⚠️ <strong>Perhatian:</strong> Status &quot;Diterima&quot; akan langsung mengupdate stok dan harga beli bahan baku.
           </p>
         </div>
       )}
